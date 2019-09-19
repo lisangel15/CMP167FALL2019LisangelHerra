78657f80a7130670b17389782a1cec7ba2657c86
lisangel15 <54686606+lisangel15@users.noreply.github.com>
2019-09-19 16:42:27 -0400

    Add files via upload

---
+++ MegaMillion.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ * @author Lisangel Herra
+ * 
+ */
+
+import java.util.Random; 
+import java.util.Scanner; 
+
+public class MegaMillion {
+	public static void main(String[] args) {
+		Random randGen= new Random(); 
+		
+		System.out.println(randGen.nextInt(60)+1);
+		
+		//Declare variable to hold amount of dollar 
+		int dollar; 
+		//Declare variable for cost of ticket 
+		int  ticket; 
+		//Prompt the user for the amount of dollars to bet on
+		System.out.println("How much money will you bet ? "); 
+		//store amount into amount of dollar variable 
+		Scanner in = new Scanner(dollar); 
+		dollar = in.nextInt(); 
+		
+		//
+	}
+
+}
