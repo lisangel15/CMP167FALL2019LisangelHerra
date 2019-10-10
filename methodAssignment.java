df8ecb742efc267335daa0064e7d2824ba0a707a
lisangel15 <54686606+lisangel15@users.noreply.github.com>
2019-10-10 16:41:04 -0400

    Add files via upload

---
+++ methodAssignment.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ * 
+ * @author Lisangel Herra
+ *
+ */
+
+public class methodAssignment {
+	public static void main(String[] args) {
+		
+	}
+	public static int product(int num1,int num2,int num3) {
+		return num1*num2*num3; 
+	}
+	
+	public static int sum(int num1, int num2) {
+		for(int i= num1; i <= num2; i++ ) {
+			int sum=0; 
+			sum = num1 + i; 
+		}
+		return sum;
+	}
+	
+	public static int average(int n, int count) {
+		for(int i= 1; i <n; i++) {
+			int total = total + n;  
+		}
+		return sum/n;
+	}
+
+}
