f2370a4ada11257502579463f01e7bb13acd4e53
lisangel15 <54686606+lisangel15@users.noreply.github.com>
2019-10-15 16:26:48 -0400

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,16 @@
+/*
+ * Lisangel Herra
+ */
+public class Animal {
+	 public int numLegs; 
+	 private int age; 
+	
+public void bark() {
+	System.out.println("Woof woof");
+	}
+
+public void run() {
+	System.out.println();
+	}
+
+}
